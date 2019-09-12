89a2b229e4c8c6688a8fff345ecbcd815ae797cc
Olivier Seret <os7@sanger.ac.uk>
2019-09-12 10:01:13 +0100

    RT669882 stop updating rc files

--- bakerlib/main.py
+++ bakerlib/main.py
@@ -4,7 +4,6 @@ import re
 from pkg_resources import get_distribution, DistributionNotFound
 from bakerlib.softwares import get_softwares
 from bakerlib.templating import TemplateRenderer
-from bakerlib.rc_processing import update_rc_file
 
 _logger = logging.getLogger('main')
 
@@ -21,10 +20,10 @@ def preamble():
 
 def decorate(input_dirs, output_dir, template_dir):
     _decorate(input_dirs, output_dir, TemplateRenderer.new_instance(
-        template_dir), get_softwares, update_rc_file)
+        template_dir), get_softwares)
 
 
-def _decorate(input_dirs, output_dir, renderer, retrieve_software, rc_file_updater):
+def _decorate(input_dirs, output_dir, renderer, retrieve_software):
     softwares = retrieve_software(input_dirs)
     _logger.debug("Software retrieved: %s", softwares)
     for software in softwares:
@@ -33,8 +32,6 @@ def _decorate(input_dirs, output_dir, renderer, retrieve_software, rc_file_updat
         software_directory = "%s/%s" % (software["name"], software["version"])
         wrapper_directory = "%s/wrappers" % software_directory
         output_wrapper_directory = "%s/%s" % (output_dir, wrapper_directory)
-        output_software_directory = "%s/%s" % (output_dir, software_directory)
-        rc_file_updater(output_software_directory, software["name"] + '.rc', wrapper_directory)
         for function in software['exported_functions']:
             renderer.create_script(
                 output_wrapper_directory, function["script_name"], software, function)
