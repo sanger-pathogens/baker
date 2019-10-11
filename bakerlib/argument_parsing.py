6e993bea2ff858f25b1bd6bec7fef8e81d7be0bd
Olivier Seret <os7@sanger.ac.uk>
2019-10-11 10:59:54 +0100

    RT670786 support --version

--- bakerlib/argument_parsing.py
+++ bakerlib/argument_parsing.py
@@ -1,8 +1,11 @@
 import argparse
 
 
-def new_argument_parser(decorating, factory=lambda: argparse.ArgumentParser()):
+def new_argument_parser(decorating, version, factory=lambda: argparse.ArgumentParser()):
     parser = factory()
+    
+    parser.add_argument('--version', action='version', version=version)
+
     subparsers = parser.add_subparsers(help='sub-command help')
     bake_parser = subparsers.add_parser(
         'decorate', help='Generate wrappers for all softwares')
@@ -13,4 +16,6 @@ def new_argument_parser(decorating, factory=lambda: argparse.ArgumentParser()):
     bake_parser.add_argument(
         '--templates','-t', dest='template_dir', required=True, help='directory containing the jinja2 templates')
     bake_parser.set_defaults(func=decorating)
+
+
     return parser
